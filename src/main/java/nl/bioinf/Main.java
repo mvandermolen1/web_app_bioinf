e2bfb69c8732cbdd0887ede035bc3a6cad0f4179
ishofstede <i.s.hofstede@st.hanze.nl>
2023-11-27 10:20:54 +0100

    initial commit

--- src/main/java/nl/bioinf/Main.java
+++ src/main/java/nl/bioinf/Main.java
@@ -6,6 +6,7 @@ public class Main {
     public static void main(String[] args) {
         // Press Alt+Enter with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        System.out.println("Hello and welcome!");
+        System.out.println("lmao");
     }
 }
