cb50f87a82af31bf4515e87cbba2204ea73bf1f2
ishofstede <i.s.hofstede@st.hanze.nl>
2023-12-04 12:55:33 +0100

    Fixed the error by extending to class.

--- src/main/java/nl/bioinf/game/ScenarioHandler.java
+++ src/main/java/nl/bioinf/game/ScenarioHandler.java
@@ -5,7 +5,7 @@ public class ScenarioHandler {
         //request for player
         System.out.println("Bring me 3 of those flasks please.");
         //player input handling
-        int flasks = getUserInput("Enter the number of flasks you need");
+        int flasks = InputHelper.getUserInput("Enter the number of flasks you need");
         if (flasks == 3){
             System.out.println("Thank you!");
         } else {
