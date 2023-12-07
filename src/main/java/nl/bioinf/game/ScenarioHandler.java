4b5603d16e1e924ce9aaf948c099c849bbd460d4
ishofstde <i.s.hofstede@st.hanze.nl>
2023-12-07 13:17:19 +0100

    Basic setup for a text-based game with button layout in html and logic in javascript. Disabled the Java backend for now so the code actually runs without it.

--- src/main/java/nl/bioinf/game/ScenarioHandler.java
+++ src/main/java/nl/bioinf/game/ScenarioHandler.java
@@ -5,12 +5,12 @@ public class ScenarioHandler {
         //request for player
         System.out.println("Bring me 3 of those flasks please.");
         //player input handling
-        int flasks = InputHelper.getUserInput("Enter the number of flasks you need");
-        if (flasks == 3){
-            System.out.println("Thank you!");
-        } else {
-            System.out.println("This is not the right amount!");
-        }
+//        int flasks = InputHelper.getUserInput("Enter the number of flasks you need");
+//        if (flasks == 3){
+//            System.out.println("Thank you!");
+//        } else {
+//            System.out.println("This is not the right amount!");
+//        }
     }
 
     //other methods here.
